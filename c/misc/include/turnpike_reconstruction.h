3b07eec3a4251a68903424f6d65d341c2f896c38
Bear Kun <bear_kun@outlook.com>
2025-09-06 01:56:43 +0800

    [^-^] change to public

--- c/misc/include/turnpike_reconstruction.h
+++
@@ -1,9 +0,0 @@
-#ifndef TURNPIKE_RECONSTRUCTION_H
-#define TURNPIKE_RECONSTRUCTION_H
-
-typedef int DistanceType;
-
-void reconstructTurnpike(DistanceType distances[], DistanceType points[],
-                         int npts);
-
-#endif // TURNPIKE_RECONSTRUCTION_H
